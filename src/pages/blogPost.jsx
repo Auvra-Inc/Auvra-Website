971fdb44fcbaf28fc1e21a9087c7475a6ae81d4f
JCE <auvrahq@gmail.com>
2026-05-25 07:09:18 -0400

    Update blogPost.jsx

--- src/pages/blogPost.jsx
+++ src/pages/blogPost.jsx
@@ -136,8 +136,8 @@ export default function BlogPost() {
           </div>
           
           <div className="relative z-10 flex flex-col justify-between h-full">
-            {/* LOGO - MOVED UP, INCREASED TO 250px, CENTERED */}
-            <div className="pt-8 pb-4 flex justify-center items-center">
+            {/* LOGO - MOVED UP to exact position where text was (reduced top padding) */}
+            <div className="pt-2 pb-4 flex justify-center items-center">
               <Link to="/" className="flex justify-center">
                 <img 
                   src="/Logo-png.PNG" 

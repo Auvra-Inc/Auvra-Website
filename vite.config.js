a7500188d3905836740499eacb13f7c63b82ea9d
vincen-coder <omoseebivincent185@gmail.com>
2026-06-01 20:04:56 +0100

    created eacah html file for each page for vercel to check for manually

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),],
-})
+  plugins: [react(),
+    tailwindcss()
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        institutional: resolve(__dirname, 'institutional-access.html')
+      }
+    }
+  }
+});

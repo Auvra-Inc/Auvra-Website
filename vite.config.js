7fe315d7f93535e6fc40994213558ac15d33c58b
vincen-coder <omoseebivincent185@gmail.com>
2026-06-01 20:11:07 +0100

    fixed the build mistkae forgot to add /vite to the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from 'tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
   ],
   build: {
     rollupOptions: {

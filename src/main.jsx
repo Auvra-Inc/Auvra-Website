2f098121851ebd0726695e83d3e3bda3ca3c0bfa
vincen-coder <omoseebivincent185@gmail.com>
2026-06-01 19:30:00 +0100

    remved react-sanp

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { hydrateRoot, createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  // This runs for the static snapshot (for WhatsApp bots)
-  hydrateRoot(
-    rootElement,
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-} else {
-  // This runs normally in development
-  const root = createRoot(rootElement);
-  root.render(
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-}
+createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
